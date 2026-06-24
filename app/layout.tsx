5f705fbddc1143b3b43b00d45a610b49e446fb69
egarrisxn <egarrisxn@gmail.com>
2026-06-23 21:41:17 -0400

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
-        {" "}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
